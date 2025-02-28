5c0c4d67ee20ff1c815b778f6b9e87ab6dcb6815
Petrov Andrey <4ndrew1337@mail.ru>
2025-02-28 19:48:07 +0300

    added layers - added layers in main

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,11 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/andreyxaxa/posts_comments_service/configs"
 	"github.com/andreyxaxa/posts_comments_service/graph"
+	"github.com/andreyxaxa/posts_comments_service/internal/db"
+	"github.com/andreyxaxa/posts_comments_service/internal/gateway"
+	"github.com/andreyxaxa/posts_comments_service/internal/gateway/postgres"
 	resolvers "github.com/andreyxaxa/posts_comments_service/internal/server/graphql"
+	"github.com/andreyxaxa/posts_comments_service/internal/service"
 	"github.com/andreyxaxa/posts_comments_service/pkg/logger"
 )
 
@@ -28,13 +32,47 @@ func main() {
 		logger.Error.Fatal(err.Error())
 	}
 
-	port := os.Getenv("PORT")
+	logger.Info.Print("Connectin to Postgres")
+	options := db.PostgresOptions{
+		Name:     os.Getenv("DB_DBNAME"),
+		Post:     os.Getenv("DB_PORT"),
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+	}
 
-	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolvers.Resolver{}}))
+	logger.Info.Print(options)
+
+	postgresDB, err := db.NewPostgresDB(options)
+	if err != nil {
+		logger.Error.Fatalf(err.Error())
+	}
+
+	var gateways *gateway.Gateways
+
+	logger.Info.Print("Creating Gateways")
+	logger.Info.Print("USE_IN_MEMORY = ", os.Getenv("USE_IN_MEMORY")) // Добавь
+	if os.Getenv("USE_IN_MEMORY") == "true" {
+		// TODO: inmemory strg
+	} else {
+		posts := postgres.NewPostsPostgres(postgresDB)
+		comments := postgres.NewCommentsPostgres(postgresDB)
+		gateways = gateway.NewGateways(posts, comments)
+	}
+
+	logger.Info.Print("Creating services")
+	services := service.NewServices(gateways)
+
+	logger.Info.Print("Creating graphQL server")
+	port := os.Getenv("PORT")
+	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolvers.Resolver{
+		Posts:    services.Posts,
+		Comments: services.Comments,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", server)
 
-	logger.Info.Print("Connect to http://localhost:%s/ for GraphQL playground", port)
+	logger.Info.Printf("Connect to http://localhost:%s/ for GraphQL playground", port)
 	logger.Error.Fatal(http.ListenAndServe(":"+port, nil))
 }
